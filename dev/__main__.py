8371ee553211859ffff0465f0e87856cd94bec50
shawn@Ultron <shawn.gerrard@gmail.com>
2022-02-22 09:55:48 +1300

    Rolled back previous Helm Chart deployment code in main.py due to error 'exec user process caused: exec format error'

--- dev/__main__.py
+++ dev/__main__.py
@@ -6,7 +6,7 @@ Creating a Kubernetes Deployment for Lit Republic web services DEV environment
 import pulumi
 from pulumi_kubernetes.apps.v1 import Deployment
 from pulumi_kubernetes.core.v1 import Service
-from pulumi_kubernetes.helm.v3 import Chart, LocalChartOpts
+#from pulumi_kubernetes.helm.v3 import Chart, LocalChartOpts
 
 # --
 # Minikube does not implement services of type `LoadBalancer` and so requires the 
@@ -19,8 +19,8 @@ config = pulumi.Config()
 is_minikube = config.require_bool("isMinikube")
 
 # Set the deployment name 
-#app_name = "nginx"
-#app_labels = { "app": app_name }
+app_name = "nginx"
+app_labels = { "app": app_name }
 
 nginx_ingress = Chart(
     "nginx-ingress",
@@ -30,40 +30,41 @@ nginx_ingress = Chart(
 )
 
 
-# # Define an Nginx deployment for ingress control
-# deployment = Deployment(
-#     app_name,
-#     spec={
-#         "selector": { "match_labels": app_labels },
-#         "replicas": 1,
-#         "template": {
-#             "metadata": { "labels": app_labels },
-#             "spec": { "containers": [{ "name": app_name, "image": app_name }] }
-#         }
-#     })
+# Define an Nginx deployment for ingress control
+deployment = Deployment(
+    app_name,
+    spec={
+        "selector": { "match_labels": app_labels },
+        "replicas": 1,
+        "template": {
+            "metadata": { "labels": app_labels },
+            "spec": { "containers": [{ "name": app_name, "image": app_name }] }
+        }
+    }
+)
 
-# # Allocate an IP to the Deployment
-# frontend = Service(
-#     app_name,
-#     metadata={
-#         "labels": deployment.spec["template"]["metadata"]["labels"],
-#     },
-#     spec={
-#         "type": "ClusterIP" if is_minikube else "LoadBalancer",
-#         "ports": [{ "port": 80, "target_port": 80, "protocol": "TCP" }],
-#         "selector": app_labels,
-#     }
-# )
+# Allocate an IP to the Deployment
+frontend = Service(
+    app_name,
+    metadata={
+        "labels": deployment.spec["template"]["metadata"]["labels"],
+    },
+    spec={
+        "type": "ClusterIP" if is_minikube else "LoadBalancer",
+        "ports": [{ "port": 80, "target_port": 80, "protocol": "TCP" }],
+        "selector": app_labels,
+    }
+)
 
 # Get the public IP of the deployment
-#result = None
-#if is_minikube:
-#    result = frontend.spec.apply(lambda v: v["cluster_ip"] if "cluster_ip" in v else None)
-#else:
-#    ingress = frontend.status.apply(lambda v: v["load_balancer"]["ingress"][0] if "load_balancer" in v else None)
-#    if ingress is not None:
-#        result = ingress.apply(lambda v: v["ip"] if "ip" in v else v["hostname"])
+result = None
+if is_minikube:
+    result = frontend.spec.apply(lambda v: v["cluster_ip"] if "cluster_ip" in v else None)
+else:
+    ingress = frontend.status.apply(lambda v: v["load_balancer"]["ingress"][0] if "load_balancer" in v else None)
+    if ingress is not None:
+        result = ingress.apply(lambda v: v["ip"] if "ip" in v else v["hostname"])
 
 # Output public IP and deployment name
-#pulumi.export("name", deployment.metadata["name"])
-#pulumi.export("ip", result)
+pulumi.export("name", deployment.metadata["name"])
+pulumi.export("ip", result)
