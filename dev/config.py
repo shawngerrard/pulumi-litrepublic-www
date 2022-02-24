5be37d99ff669b4bebe40fb40e0d956a87d10bdc
shawn@Ultron <shawn.gerrard@gmail.com>
2022-02-24 23:34:04 +1300

    Corrected typo in config.py

--- dev/config.py
+++ dev/config.py
@@ -13,7 +13,7 @@ config = {
     "publicSubnetIds": infraStackRef.get_output("publicSubnetIds"),
 
     # Obtain Kubernetes cluster attributes
-    "kubeconfig": infraStackRef.get_output("kubeconfig"),
+    "kubeconfig": infraStackRef.get_output("kubeConfig"),
     "clusterName": infraStackRef.get_output("clusterName"),
     "securityGroupIds": infraStackRef.get_output("securityGroupIds"),
     "clusterSvcsNamespaceName": infraStackRef.get_output("clusterSvcsNamespaceName"),
